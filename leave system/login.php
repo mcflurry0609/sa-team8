ad679c7de4bc8e574f180d73ce0fe2c49a66f92d
Heng-I Lam <henrylambb@gmail.com>
2024-04-25 14:05:12 +0800

    登入php/有根據身份別導向不同頁面

---
+++ leave system/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        session_start();
+        $user_id=$_POST['id'];
+        $password=$_POST['password'];
+
+        $link=mysqli_connect('localhost','root');
+        mysqli_select_db($link,'leave');
+        $sql="select distinct * from users where user_id='$user_id' and password='$password'";
+        //echo $sql;
+        $result=mysqli_query($link,$sql);
+        if($row=mysqli_fetch_assoc($result)){
+            $_SESSION['user_id']=$row['user_id'];
+            $_SESSION['user_name']=$row['user_name'];
+            $_SESSION['role']=$row['role'];
+            if ($row['role']==['學生']){
+                $redirect_url = "record.html";
+            }
+            else {
+                $redirect_url = "review.php";
+            }
+            
+            header("Location: $redirect_url");
+            exit; 
+        }else{
+    ?>
+        <script>
+            alert('查無此帳號與密碼');
+            history.back();
+        </script>
+    <?php
+        }
+    ?>
+
+</body>
+</html>
