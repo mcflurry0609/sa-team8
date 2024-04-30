58894ffead647b4e59f87f2fe2114b1b4a91afd6
Heng-I Lam <henrylambb@gmail.com>
2024-04-30 10:46:36 +0800

    no problem

---
+++ leave system/updatestatus.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+if(isset($_GET['id']) && isset($_GET['action'])) {
+    
+    $application_id = $_GET['id'];
+    $action = $_GET['action']; 
+
+    
+    $link=mysqli_connect('localhost','root');
+    mysqli_select_db($link,'leave');
+
+    
+    if($action === 'accept') {
+        $new_status = '已批准';
+    } elseif ($action === 'reject') {
+        $new_status = '已拒絕';
+    } else {
+        echo "批准失敗，請重新批准";
+        header("Location: review.php");
+    }
+
+    
+    $update_query = "UPDATE applications SET status = '$new_status' 
+                    WHERE application_id = $application_id";
+    $update_result = mysqli_query($link, $update_query);
+
+    
+    if($update_result) {
+        header("Location: review.php");
+        exit(); 
+    } else {
+        echo "批准失敗，請重新批准";
+        header("Location: review.php");
+    }
+
+    
+} else {
+    echo "批准失敗，請重新批准";
+    header("Location: review.php");
+}
+?>
