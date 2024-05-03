cbe8edf96a5f0118847477102ffc8549340b5da2
Heng-I Lam <henrylambb@gmail.com>
2024-05-03 21:59:40 +0800

    沒輸入節次可以送出申請解決了

--- leave system/applying.php
+++ leave system/applying.php
@@ -19,21 +19,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $course_id = $_POST['course'];
     $reason = $_POST['reason'];
     $periods = $_POST['periods'];
-$Period = "";
-foreach ($periods as $period){
-    $Period.=$period;
-}
-
+    $Period = "";
+    if (empty($periods)) {
+        echo "<script>alert('請選擇節次後再送出申請！'); window.location.href = 'apply.php';</script>";
+        exit; // 停止执行后续代码
+    }
+    foreach ($periods as $period) {
+        $Period .= $period;
+    }
 
     // 处理上传的文件
     $target_dir = "uploads/"; // 上传文件的目录
     $target_file = $target_dir . basename($_FILES["proof"]["name"]);
     $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
     // 如果文件上传时出现问题
     if ($uploadOk == 0) {
-        echo "抱歉，文件未上传.";
+        echo "<script>alert('文件未上傳！'); window.location.href = 'apply.php';</script>";
     // 如果一切顺利，尝试上传文件
     } else {
         if (move_uploaded_file($_FILES["proof"]["tmp_name"], $target_file)) {
@@ -41,17 +44,15 @@ foreach ($periods as $period){
             $sql = "INSERT INTO applications (user_id, course_id, category_id, date, reason, doc_name,periods)
             VALUES ('{$_SESSION['user_id']}', '$course_id', '$category_id', '$date', '$reason', '{$target_file}', '$Period')";
             if ($link->query($sql) === TRUE) {
-                echo "<script>alert('申請已成功送出！'); window.location.href = 'record.php';</script>";
-
+                echo "<script>alert('申请已成功提交！'); window.location.href = 'record.php';</script>";
             } else {
                 echo "Error: " . $sql . "<br>" . $link->error;
             }
         } else {
-            echo "抱歉，文件上传失败.";
+            echo "<script>alert('文件上傳失敗！'); window.location.href = 'apply.php';</script>";
         }
     }
 
     // 关闭数据库连接
     $link->close();
 }
-?>
