543ab0273e939d89e4d677cba1e3fbc6420e4382
Heng-I Lam <henrylambb@gmail.com>
2024-05-08 16:22:48 +0800

    UTF-8編碼

--- leave system/statusemail.php
+++ leave system/statusemail.php
@@ -10,6 +10,7 @@ require 'vendor/autoload.php';
 
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
 
 try {
     //Server settings
@@ -22,12 +23,12 @@ try {
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('fjuonlineleavesystem@gmail.com', mb_encode_mimeheader('輔仁大學線上請假系統通知','UTF-8'));
+    $mail->setFrom('fjuonlineleavesystem@gmail.com', '輔仁大學線上請假系統通知');
     $mail->addAddress($user_email);     //Add a recipient
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = mb_encode_mimeheader('請假申請狀態更新', 'UTF-8');
+    $mail->Subject = '請假申請狀態更新';
     $mail->Body    = '您的請假申請狀態已經更新為 ' . $new_status . '。請假課程：' . $course_name . '，請假日期：' . $date . '，請假節次：' . $periods . '，送出申請的時間：' . $apply_time;
     $mail->AltBody = '您的請假申請狀態已經更新為 ' . $new_status . '。請假課程：' . $course_name . '，請假日期：' . $date . '，請假節次：' . $periods . '，送出申請的時間：' . $apply_time;
 
