e1f34937db8ffb57b70c3a2c48f0c3fa4e1cef29
carp1di1m <166577333+carp1di1m@users.noreply.github.com>
2024-05-21 15:25:29 +0800

    Add files via upload

--- leave system/person.php
+++ leave system/person.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,53 +8,86 @@
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="pic/logoo.jpg" />
     <!-- CSS -->
-    <link href="css/person.css" rel="stylesheet" />
+    <link href="css/apply.css" rel="stylesheet" />
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/2261b58659.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
+    <style>
+        .person {
+            position: absolute;
+            top: 600px;
+            left: 43%;
+            right: 43%;
+            display: inline-block;
+            padding: 10px 10px;
+            font-size: 16px;
+            font-weight: bold;
+            text-align: center;
+            text-decoration: none;
+            cursor: pointer;
+            border: 2px solid #333;
+            border-radius: 5px;
+            background-color: #fff;
+            color: #333;
+        }
+
+        .formgap{
+            margin-bottom: 20px;
+        }
+
+        .check {
+            display: flex;
+            gap: 0px; /* 調整兩個選項之間的間距 */
+        }
+
+        .check input[type="radio"],
+        .check label {
+            margin-right: 10px; /* 調整每個選項內的間距 */
+        }
+
+
+    </style>
     <div class="layout">
         <div class="wrapper">
             <h2>個人頁面</h2>
             <form class="form" method="post" action="record.php">
-                <div class="formrow">
-                <div class="reason">
-                        <div class="title">
-                            姓名
-                        </div>
+                <div class="">
+                    <div class="name formgap">
+                        <div class="title">姓名</div>
                         <div class="input">
-                            <textarea class="inputbox textarea" placeholder="請填寫姓名" maxlength="10" name="reason" disabled>魏一凡</textarea>
+                            <input type="text" class="inputbox" name="user_name">
                         </div>
-                        <br>
-                        <br>
-                        <div class="title">
-                            學號
-                        </div>
-                        <div class="input">
-                            <textarea class="inputbox textarea" placeholder="請填寫學號" maxlength="9" name="reason" disabled>410401428</textarea>
-                        </div>
-                        <br>
-                        <br>
-                        <div class="title">
-                            職稱
-                        </div>
-                        <div class="input">
-                            <textarea class="inputbox textarea" placeholder="請填寫職稱" maxlength="2" name="reason" disabled>學生</textarea>
-                        </div>
-                        <br>
-                        <br>
-                        <div class="title">
-                            email
-                        </div>
-                        <div class="input">
-                            <textarea class="inputbox textarea" placeholder="請填寫信箱" maxlength="20" name="reason" required>wyfbdm18@gmail.com</textarea>
-                        </div>
-                        <br>
-                        <br>
-                        <button type="submit" class="person" id="submitBtn">儲存</button>
                     </div>
-</div>
-</form>
-</div>
-</div>
+                    <div class="id formgap">
+                        <div class="title">學號</div>
+                        <div class="input">
+                            <input type="text" class="inputbox" name="user_id">
+                        </div>
+                    </div>
+                    <div class="role formgap">
+                        <div class="title">身分</div>
+                        <div class="input">
+                            <input type="text" class="inputbox" name="role">
+                        </div>
+                    </div>
+                    <div class="notify formgap">
+                        <div class="title">是否希望收到通知</div>
+                        <div class="check">
+                            <input type="radio" name="" id="on"><label for="on">是</label>
+                            <input type="radio" name="" id="off"><label for="off">否</label>
+                        </div>
+                    </div>
+                    <div class="gmail formgap">
+                        <div class="title">Gmail</div>
+                        <div class="input">
+                            <input type="email" class="inputbox" name="gmail">
+                        </div>
+                    </div>
+                    <button type="submit" class="person" id="submitBtn">儲存</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
